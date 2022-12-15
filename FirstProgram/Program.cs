a6197df08b542dac4282a6b58225610bf4d0896d
kimsm23236 <kimsm23236@gmail.com>
2022-12-15 16:43:00 +0900

    second commit

--- FirstProgram/Program.cs
+++ FirstProgram/Program.cs
@@ -11,6 +11,8 @@ namespace FirstProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+
         }
     }
 }
